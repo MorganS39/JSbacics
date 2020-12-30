bff32e2b312f2f8acf1043695a0b2355d75f0cde
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2020-12-29 14:04:51 -1000

    case sensitivity training

--- test.js
+++ test.js
@@ -17,3 +17,12 @@ var f = "I am a";
 d = d + 1;
 e = e + 5;
 f = f + " String!";
+
+// Case sensitivity training "be more sensitive"
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
