6bc16cf93b6808703a748f0f1fc9867ad80f75e3
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2020-12-29 15:55:44 -1000

    learned .shift() and .unshift() properties

--- test.js
+++ test.js
@@ -138,10 +138,14 @@ myArray[0] = 45;
 var arr = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 var data = arr[2][1];
 
-// Push function manipulation (add)
+// Push function manipulation (add) to add to the end
 var arr1 = [["John", 23], ["cat", 2]];
 arr1.push(["dog", 3]);
+// unshift() adds to the front of a function
+arr1.unshift(["Paul", 35]);
 
 // Pop function manipulation (subtract)
 var removedFromMyArray = arr1.pop();
 // this value should now be either 2 or 3 depending if the .push applied first.
+// pop removes the last function while .shift removes the first. example:
+var removedFromArr1 = arr1.shift();
