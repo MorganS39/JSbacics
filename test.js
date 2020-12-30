b0ed47ee7ae7da3f2cfc5387dabde6d70b1d7367
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2020-12-29 14:01:08 -1000

    learned how to initialize variables

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// Learning variables and setting numeric values
 var a;
 
 a = 7;
@@ -7,3 +8,12 @@ var b;
 b = a;
 
 var c = 9;
+
+// Understanding unitialized variables
+var d = 5;
+var e = 10;
+var f = "I am a";
+
+d = d + 1;
+e = e + 5;
+f = f + " String!";
