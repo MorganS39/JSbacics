e1cf1efa1371eadaecbfedfb1072a54655a63a19
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 15:50:12 -1000

    switch statements continued

--- test.js
+++ test.js
@@ -405,3 +405,24 @@ function caseInSwitch(val) {
     return answer;
 }
 caseInSwitch(1);
+
+// switch continued. (getting clearer)
+function switchOfStuff(val) {
+    var answer = "";
+    switch(val) {
+      case "a":
+        return "apple";
+        break;
+      case "b":
+        return "bird";
+        break;
+      case "c":
+        return "cat";
+        break;
+      default:
+        return "stuff";
+        break;
+    }
+    return answer;
+}
+switchOfStuff(1);
