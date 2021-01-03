090445306cd197b058d136cdde8d1629acf8234c
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 14:51:25 -1000

    logical And operator &&

--- test.js
+++ test.js
@@ -247,10 +247,10 @@ testStrictNotEqual(10);
 
 // Greater than operator (>)
 function testGreaterThan(val) {
-    if (val > 100) {  // Change this line
+    if (val > 100) { 
       return "Over 100";
     }
-    if (val > 10) {  // Change this line
+    if (val > 10) {
       return "Over 10";
     }
     return "10 or Under";
@@ -259,11 +259,11 @@ testGreaterThan(10);
 
 // Greater than equal to operator (>=)
 function testGreaterOrEqual(val) {
-    if (val >= 20) {  // Change this line
+    if (val >= 20) { 
       return "20 or Over";
     }
   
-    if (val >= 10) {  // Change this line
+    if (val >= 10) { 
       return "10 or Over";
     }
   
@@ -273,11 +273,11 @@ testGreaterOrEqual(10);
 
 // Less than operator (<)
 function testLessThan(val) {
-    if (val < 25) {  // Change this line
+    if (val < 25) { 
       return "Under 25";
     }
   
-    if (val < 55) {  // Change this line
+    if (val < 55) { 
       return "Under 55";
     }
   
@@ -287,14 +287,23 @@ testLessThan(10);
 
 // Less than equal to (<=)
 function testLessOrEqual(val) {
-    if (val <= 12) {  // Change this line
+    if (val <= 12) {
       return "Smaller Than or Equal to 12";
     }
   
-    if (val <= 24) {  // Change this line
+    if (val <= 24) { 
       return "Smaller Than or Equal to 24";
     }
   
     return "More Than 24";
 }
 testLessOrEqual(10);
+
+// Logical And operator (&&)
+function testLogicalAnd(val) {
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    }
+    return "No";
+}
+testLogicalAnd(10);
