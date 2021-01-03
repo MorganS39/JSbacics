60f64941185d7eab0911f10d15eca0f195622bfc
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 15:46:53 -1000

    switch statements

--- test.js
+++ test.js
@@ -384,3 +384,24 @@ function golfScore(par, strokes) {
   }
 }
 golfScore(5, 4);
+
+// Switch statements (still a bit unclear)
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+      case 1:
+        return "alpha";
+        break;
+      case 2:
+        return "beta";
+        break;
+      case 3:
+        return "gamma";
+        break;
+      case 4:
+        return "delta";
+        break;
+    }
+    return answer;
+}
+caseInSwitch(1);
