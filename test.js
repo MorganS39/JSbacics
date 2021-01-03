7e743e5b54370e4be56084467ad17d85090ab424
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 15:00:45 -1000

    Else If statements

--- test.js
+++ test.js
@@ -329,3 +329,17 @@ function testElse(val) {
     return result;
 }
 testElse(4);
+
+// Else If statements
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    }
+    else if (val < 5) {
+        return "Smaller than 5";
+    }
+    else {
+        return "Between 5 and 10";
+    }    
+}
+testElseIf(7);
