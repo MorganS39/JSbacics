25dfdb112698206109ca00dbd7ee63e63ed3da03
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 14:43:23 -1000

    greater than > operator

--- test.js
+++ test.js
@@ -244,3 +244,15 @@ function testStrictNotEqual(val) {
     return "Equal";
 }
 testStrictNotEqual(10);
+
+// Greater than operator >
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+    return "10 or Under";
+}
+testGreaterThan(10);
