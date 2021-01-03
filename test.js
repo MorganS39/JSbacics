487f7170c883e7ce4e5c34aa742e34093bbc7657
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 15:19:44 -1000

    chaining IF ELSE statements in order

--- test.js
+++ test.js
@@ -343,3 +343,23 @@ function testElseIf(val) {
     }    
 }
 testElseIf(7);
+
+// Chaining If Else statements in proper order
+function testSize(num) {
+    if (num < 5) {
+      return "Tiny";
+    }
+    else if (num < 10) {
+      return "Small";
+    }
+    else if (num < 15) {
+      return "Medium";
+    }
+    else if (num < 20) {
+      return "Large";
+    }
+    else {
+      return "Huge";
+    }
+}
+testSize(7);
