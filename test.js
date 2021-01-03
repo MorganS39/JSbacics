830c0d064940d46d6789c27d47ba03879c0e2b2b
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 14:40:38 -1000

    Strict inequality operator !==

--- test.js
+++ test.js
@@ -235,3 +235,12 @@ function testNotEqual(val){
     return "Equal";
 }
 testNotEqual(10);
+
+// Strict Inequality operator (!==)
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+testStrictNotEqual(10);
