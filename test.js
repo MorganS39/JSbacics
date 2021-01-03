63372cb78f54249eccb81e8ad4afb62e3e605c97
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-02 14:46:53 -1000

    Less than operator <

--- test.js
+++ test.js
@@ -270,3 +270,17 @@ function testGreaterOrEqual(val) {
     return "Less than 10";
 }
 testGreaterOrEqual(10);
+
+// Less than operator (<)
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+}
+testLessThan(10);
