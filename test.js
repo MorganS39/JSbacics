6269576a1790c76a5097f9110b7bd47580e2ecc9
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-04 12:35:11 -1000

    Accessing object properties with variables

--- test.js
+++ test.js
@@ -533,3 +533,12 @@ var testObj = {
 };
 var entreeValue = testObj["an entree"];
 var drinkValue = testObj["the drink"];
+
+// Accessing Object properties with variables
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+var playerNumber = 16;
+var player = testObj[playerNumber];
