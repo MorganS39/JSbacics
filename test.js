f9bcf7a4cf0810e1e9f2988efcc455acf20e1193
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-04 13:21:52 -1000

    Accessing nested arrays

--- test.js
+++ test.js
@@ -634,3 +634,24 @@ var myStorage = {
   }
 };
 var gloveBoxContents = myStorage.car.inside["glove box"];
+
+// Accessing nested arrays
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+var secondTree = myPlants[1].list[1];
